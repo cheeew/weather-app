ca5f502a088ebf2cc5d0581fd0950a7ac0aaffb0
Matthew Martinez <hellomattmartinez@gmail.com>
2018-09-22 02:16:33 -0700

    configure google geocode and darksky api

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-const geocodeApi = 'AIzaSyBWIsU_qcYzM8z_knUgr99-nnhQk4dYBkk';
-const findMeButton = document.querySelector('.ask-location');
+const textField = document.querySelector('.enter-city');
+const findMeButton = document.querySelector('.track-my-location');
 const locationResult = document.querySelector('.location-result');
+const form = document.querySelector('form');
 
-const url = "https://maps.googleapis.com/maps/api/geocode/json";
-const params = `?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${geocodeApi}`;
+const googleApiKey = 'AIzaSyBWIsU_qcYzM8z_knUgr99-nnhQk4dYBkk';
+const googleUrl = "https://maps.googleapis.com/maps/api/geocode/json";
 
-function showPosition(position) {
-  let latitude = position.coords.latitude, 
+const darkskyApiKey = '96fd99f683a5ebbcb2a8b68bd67f683e/';
+const darkskyUrl = "https://api.darksky.net/forecast/";
+
+function findCityCoords(e) {
+  e.preventDefault();
+  const userInput = textField.value.trim();
+  const params = `?address=${userInput}&key=${googleApiKey}`;
+  
+  fetch(googleUrl + params)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const coords = `${data.results[0].geometry.location.lat},${data.results[0].geometry.location.lng}`;
+      return fetchJsonp(darkskyUrl + darkskyApiKey + coords);
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      locationResult.innerHTML = `The temperature in the ${data.timezone} timezone is ${data.currently.temperature}`;
+    });
+}
+
+function reportWeather(position) {
+  let latitude = position.coords.latitude,
   longitude = position.coords.longitude;
-  console.log(position);
-  locationResult.innerHTML = `${latitude}, ${longitude}`;
+
+  const getWeather = () => {
+    const darkskyParams = `${darkskyApiKey}${latitude},${longitude}`;
+  
+    fetchJsonp(`${darkskyUrl}${darkskyParams}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        locationResult.innerHTML = `The temperature in the ${data.timezone} timezone is ${data.currently.temperature}`;
+      });
+  }
+  return getWeather();
 }
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(reportWeather);
   } else {
     locationResult.innerHTML = "Geolocation isn't supported by this browser";
   }
 }
 
+form.addEventListener('submit', findCityCoords);
 findMeButton.addEventListener('click', getLocation);
