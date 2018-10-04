226809f1aa67a8bdb11e5fdf373bb972c33c57e3
Matthew Martinez <hellomattmartinez@gmail.com>
2018-10-04 00:35:15 -0700

    fix environment variables

--- src/js/config.js
+++ src/js/config.js
@@ -1,9 +1,9 @@
 export const config = {
   googleUrl : 'https://maps.googleapis.com/maps/api/geocode/json',
   googlePlacesURL : 'https://maps.googleapis.com/maps/api/place/autocomplete/json?',
-  googleApiKey :  process.env.GOOGLE_API_KEY,
+  googleApiKey : GOOGLE_API_KEY,
   
   darkskyUrl : 'https://api.darksky.net/forecast/',
-  darkskyApiKey : process.env.DARK_SKY_API_KEY
+  darkskyApiKey : DARK_SKY_API_KEY
 
 }
